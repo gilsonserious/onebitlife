8e400ce7a4b3e2917934c6c8cf9d6b3d6161a0ae
gilsonserious <gilsonglima00@gmail.com>
2022-12-20 09:36:14 -0300

    Estilização da página start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { ScrollView, View, Text, Image } from "react-native";
+import { ScrollView, View, Text, Image, StyleSheet } from "react-native";
 
 export default function Start() {
 
     return (
-        <View>
+        <View style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                    <Image
+                <View style={{alignItems: "center"}}>
+                    <Image style={styles.logo}
                         source={require("../../assets/icons/logo3.png")}
                     />
-                    <Text>
-                        Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o melhor nível.
+                    <Text style={styles.description}>
+                        Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível.
 
                     </Text>
                 </View>
@@ -20,3 +20,23 @@ export default function Start() {
     )
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(21, 21, 21, 0.98)',
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+    description: {
+        color: "#FFFFFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    }
+})
--- src/Routes/AllPages.jsx
+++ src/Routes/AllPages.jsx
@@ -1,5 +1,5 @@
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Start from '../Pages/Start';
 
