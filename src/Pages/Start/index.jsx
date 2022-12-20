6594217cfab395dbf64a50454bff0520e66b491f
gilsonserious <73702752+gilsonserious@users.noreply.github.com>
2022-12-20 15:37:27 -0300

    Atualização robô

---
+++ src/Pages/Start/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { ScrollView, View, Text, Image, StyleSheet } from "react-native";
+import LifeStatus from "../../Components/Common/LifeStatus";
+
+export default function Start() {
+
+    return (
+        <View style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ alignItems: "center" }}>
+                    <Image style={styles.logo}
+                        source={require("../../assets/icons/logo3.png")}
+                    />
+
+                    <LifeStatus />
+
+                    <Text style={styles.description}>
+                        Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível.
+
+                    </Text>
+                </View>
+            </ScrollView>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(21, 21, 21, 0.98)',
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+    description: {
+        color: "#FFFFFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    }
+})
