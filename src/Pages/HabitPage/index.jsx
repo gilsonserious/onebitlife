918537e7a7d6ce9e8a9f0c6983b8e57c6539faa5
gilsonserious <73702752+gilsonserious@users.noreply.github.com>
2022-12-30 13:19:58 -0300

    Update e Exclude Buttons

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -13,6 +13,8 @@ import Notification from "../../Components/HabitPage/Notification";
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
 import TimeDatePicker from "../../Components/HabitPage/TimeDataPicker";
+import { UpdateExcludeButtons } from "../../Components/HabitPage/UpdateExcludeButtons/Index";
+import DefaultButton from "../../Components/Common/DefaultButton";
 
 
 
@@ -25,6 +27,48 @@ export default function HabitPage({ route }) {
     const [timeNotification, setTimeNotification] = useState();
 
     const { create, habit } = route.params;
+
+    function handleCreateHabit() {
+
+        if (
+            habitInput === undefined ||
+            frequencyInput === undefined
+        ) {
+            Alert.alert(
+                'Você precisa selecionar um hábito e frequência para continuar'
+            );
+        } else if (
+            notificationToggle === true &&
+            frequencyInput === 'Diário' &&
+            timeNotification === undefined
+        ) {
+            Alert.alert(
+                'Você precisa dizer o horário da notificação!'
+            );
+        } else if (
+            notificationToggle === true &&
+            frequencyInput === 'Diário' &&
+            dayNotification === undefined &&
+            timeNotification === undefined
+        ) {
+            Alert.alert(
+                'Você precisa dizer a frequência e o horário da notificação!'
+            );
+        } else {
+            navigation.navigate('Home', {
+                createdHabit: `Created in ${habit?.habitArea}`
+            })
+        }
+    }
+    function handleUpdateHabit() {
+        if (notificationToggle === true && !dayNotification && !timeNotification) {
+            Alert.alert('Você precisa colocar a frequência e horário da notificação')
+        } else {
+            navigation.navigate('Home', {
+                updateHabit: `Updated in ${habit?.habitArea}`,
+            });
+        }
+    }
 
     return (
         <View style={styles.container}>
@@ -71,6 +115,22 @@ export default function HabitPage({ route }) {
                             )
                         ) : null}
 
+                        {create === false ? (
+                            <UpdateExcludeButtons
+                                handleUpdate={handleUpdateHabit}
+                                habitArea={habitArea}
+                                habitInput={habitInput}
+                            />
+                        ) : (
+                            <View style={styles.configButton}>
+                                <DefaultButton
+                                    buttonText={'Criar'}
+                                    handlePress={handleCreateHabit}
+                                    width={250}
+                                    height={50}
+                                />
+                            </View>
+                        )}
                     </View>
                 </View>
             </ScrollView>
